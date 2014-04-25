f1c06941a8bcbfe651f26e57d2ef903a6db33554
James Halliday <mail@substack.net>
2014-04-25 01:17:16 -0700

    fixed

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var dot = require('robust-dot-product');
 module.exports = function (n) {
     var p0 = [ 0 ];
     var p1 = [ 1 ];
-    var c = [ 1/2 ];
+    
+    var h = 1/2;
+    var c = [ h ];
+    
     for (var i = 1; i < n; i++) {
         p0.push(0);
         p1.push(0);
-        c.push(1/(i+2));
+        var cp = c[i-1];
+        h = Math.sqrt(1 - cp*cp);
+        c.push(h /(i+2));
     }
+    
     var v0 = [], v1 = [];
     for (var i = 0; i < p0.length; i++) {
         v0.push(p0[i] - c[i]);
